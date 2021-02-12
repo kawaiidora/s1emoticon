b6dcb2bd2eab3d0551ca08dff92bc991cee03df3
Dora <kawaiidora@outlook.com>
2021-02-12 09:36:54 +0800

    remove duplicate files and renaming feature finished

--- bulk_rename.ps1
+++ bulk_rename.ps1
@@ -7,32 +7,88 @@
 # destpath 是重命名后的文件夹位置，默认为rootpath下的publish文件夹
 param (
     [string]$rootpath = $PSScriptRoot,
-    [string]$destpath = $(Join-Path $PSScriptRoot "publish"),
-    [switch]$debug = $false 
+    [string]$destpath = $(Join-Path $PSScriptRoot "publish")
 )
 
-# 修改PowerShell debug环境变量
-if ($debug) {
-    $DebugPreference = "Continue"
-}
+# Write-Debug "rootpath is: $($rootpath)"
+# Write-Debug "destpath is: $($destpath)"
 
-Write-Debug "rootpath is: $($rootpath)"
-Write-Debug "destpath is: $($destpath)"
-
-# folders 打包包含的文件夹
+# 只复制打包包含的文件夹
 $folders = "animal", "bundam", "carton", "device", "face", "goose"
 
+# 绕开“如果目标文件夹已存在，则copy-item会报错”
+if (-not (Test-Path $destpath)) {
+    foreach ($foldername in $folders) {
+        $source = Join-Path $rootpath $foldername
+        $destination = Join-Path $destpath $foldername
+        # Write-Debug "source is: $($source)"
+        # Write-Debug "destination is: $($destination)"
+        Copy-Item $source $destination -Recurse
+    }
+}
+
+# 类别和表中typeid的对应关系
+$typeid = @{
+    "animal" = 1468;
+    "bundam" = 1471;
+    "carton" = 1467;
+    "device" = 1469;
+    "face"   = 1465;
+    "goose"  = 1470;
+}
+
+# $result = Get-ChildItem -Path $(Join-Path $destpath "animal") -Name
+# Write-Debug $result[0].GetType()
+
+function NameMatchThreeDigit {
+    param (
+        [string]$filename
+    )
+    $filename -match "^[0-9][0-9][0-9]"
+}
+
+function NameMatchOneLetterThreeDigit {
+    param (
+        [string]$filename
+    )
+    $filename -match "^[A-Z][0-9][0-9][0-9]"
+}
+
+
+# Write-Debug "going to rename"
 foreach ($foldername in $folders) {
-    $source = Join-Path $rootpath $foldername
-    $destination = Join-Path $destpath $foldername
-    Write-Debug "source is $($source)"
-    Write-Debug "destination is $($destination)"
-    Copy-Item $source $destination -Recurse
+    $targetpath = Join-Path $destpath $foldername
+    # Write-Debug "targetpath is: $targetpath"
+    $filenames = Get-ChildItem -Path $targetpath -Name
+    foreach ($filename in $filenames) {
+        # $withoutletter = $filename -match "^[0-9][0-9][0-9]"
+        # $withletter = $filename -match "^[A-Z][0-9][0-9][0-9]"
+        # $withletter = NameMatchOneLetterThreeDigit $filename
+        # $withoutletter = NameMatchThreeDigit $filename
+        # Write-Debug "$filename matches withletter is $withletter"
+        # Write-Debug "$filename matches withoutletter is $withoutletter"
+        
+        $substring_index = -1;
+        if (NameMatchOneLetterThreeDigit $filename) {
+            # $newname = $filename.Substring(1, 3) + [System.IO.Path]::GetExtension($filename)
+            # Rename-Item -Path $(Join-Path $targetpath $filename) -NewName $newname
+            # continue
+            $substring_index = 1;
+        }
+        if (NameMatchThreeDigit $filename) {
+            $substring_index = 0;
+        }
+        if ($substring_index -eq -1) {
+            Write-Error "something wrong in the filename, fix it or contact script author"
+            exit
+        }
+        $newname = $filename.Substring($substring_index, 3) + [System.IO.Path]::GetExtension($filename)
+
+        # 如果编号有重复，这里就会提示文件名错误
+        try {
+        Rename-Item -Path $(Join-Path $targetpath $filename) -NewName $newname
+        } catch {
+            throw "renaming $filename got error, check the file"
+        }
+    }
 }
-
-
-# 把debug环境变量改回来
-if ($debug) {
-    $DebugPreference = "SilentlyContinue"
-}
-
